146de3342f8fafea2a0a4e1c7c1244a13bd52fe5
Demola Okesola <okesola.demola@gmail.com>
2020-12-12 20:15:13 +0100

    Well done So far

---
+++ site/templates/_header.php
@@ -0,0 +1,42 @@
+<section class="service-two service-two__service-page service-two__about-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="service-two__image">
+                    <img src="assets/images/resources/about-page-moc-1.png" alt="">
+                </div><!-- /.service-two__image -->
+            </div><!-- /.col-lg-6 -->
+            <div class="col-lg-6">
+                <div class="service-two__block">
+                    <div class="block-title-two text-left">
+                        <p>about us</p>
+                        <h3>Exclusive Agency To <br> Provide Soluation</h3>
+                    </div><!-- /.block-title-two -->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tem
+                        por incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                    <ul class="list-unstyled video-one__list">
+                        <li>
+                            <i class="far fa-check"></i>
+                            Smashing Book 6 Is Here New Frontiers In Web Design
+                        </li>
+                        <li>
+                            <i class="far fa-check"></i>
+                            Introduction To Animation And The iMessage App Store With Shruggie
+                        </li>
+                        <li>
+                            <i class="far fa-check"></i>
+                            Get Your Mobile Site Ready For The 2018 Holiday ways goods
+                        </li>
+                        <li>
+                            <i class="far fa-check"></i>
+                            Making Distributed Product Teams Work More Efficiently other
+                        </li>
+                    </ul><!-- /.list-unstyled video-one__list -->
+
+                    <a href="#" class="thm-btn">Read More <i class="fa fa-angle-double-right"></i></a>
+                    <!-- /.thm-btn -->
+                </div><!-- /.service-two__block -->
+            </div><!-- /.col-lg-5 -->
+        </div><!-- /.row -->
+    </div><!-- /.container -->
+</section><!-- /.service-two -->
